fd94f735068d068fd9228a0e22b9023fac89e243
Wafers <rsjonah@gmail.com>
2024-09-12 15:53:04 -0400

    update

--- Calculator.cs
+++ Calculator.cs
@@ -10,27 +10,39 @@ internal class Program
 
         while(true) 
         {
+            
+            Console.WriteLine("please input a simple math problem. Examples: 5*5, 124/76, 4784+272 etc.");
 
-        Console.WriteLine("please input a simple math problem. Examples: 5*5, 124/76, 4784+272 etc.");
+            //receives input from user
+            string input = Console.ReadLine();
 
-        //receives input from user
-        string input = Console.ReadLine();
-
+                
+            // program shutdown
             if (input == "exit") break;
 
-        //Looks for an operator inside of the input
-        char i = operatorDetector(input);
-        if (i == 'n')
-        {
-            Console.WriteLine("sorry, you must've not input an operator. Please try again.");
+
+
+
+
+            //creating the int array that will hold all the numbers.
+            string[] values = input.Split('/', '-', '+', '*');
+            int [] numbers = Array.ConvertAll(values, int.Parse);
+
+
+
+            //creates list of operators
+            List<char> operators = operatorDetector(input);
+
+
+            //calculation answer
+            int result = calculation(numbers, operators);
+
+            //Then uses the result from operatorDetector to actually do math ad print the result to the user.
+            Console.WriteLine(result);
+            
+
         }
-        else
-        {
-        // calculates the answer based on the input being split into an array by the operator.
-        // Then uses the result from operatorDetector to decide what it will do with the numbers.
-            Console.WriteLine(calculation(input, i));
-        }
-        }
+            
 
 
 
@@ -38,60 +50,77 @@ internal class Program
 
 
 
-        int calculation(string input, char[] operators)
+        int calculation(int[] numbers, List<char> operators)
         {
             int answer = 0;
-            string[] values = input.Split('/', '-', '+', '*');
             int bigAnswer = 0;
-            int[] numbers = new int[values.Length];
+            int secondCounter = 0;
 
-            for (int counter = 0; counter >= values.Length; counter+=2)
+            for (int counter = 0; counter < numbers.Length; counter+=2)
             {
-
-                int.TryParse(values[counter], out numbers[counter]);
-
-                char c = operators[counter];
-                switch (c)
+                char c = operators.ElementAt(secondCounter);
+                if(c != '\u0000' && numbers[counter] != 0 && secondCounter < operators.Count && counter+1 < numbers.Length)
                 {
-                    case '-':
-                        answer = numbers[counter] - numbers[counter+1]; break;
-                    case '+':
-                        answer = numbers[counter] - numbers[counter + 1]; break;
-                    case '/':
-                        answer = numbers[counter] - numbers[counter + 1]; break;
-                    case '*':
-                        answer = numbers[counter] - numbers[counter + 1]; break;
+                    switch (c)
+                    {
+                        case '-':
+                            answer = numbers[counter] - numbers[counter + 1]; break;
+                        case '+':
+                            answer = numbers[counter] + numbers[counter + 1]; break;
+                        case '/':
+                            answer = numbers[counter] / numbers[counter + 1]; break;
+                        case '*':
+                            answer = numbers[counter] * numbers[counter + 1]; break;
+                    }
 
+                } 
+                else 
+                {
+                
+                    switch (c)
+                    {
+                        case '-':
+                            answer = numbers[counter] - numbers[counter + 1]; break;
+                        case '+':
+                            answer = numbers[counter] + numbers[counter + 1]; break;
+                        case '/':
+                            answer = numbers[counter] / numbers[counter + 1]; break;
+                        case '*':
+                            answer = numbers[counter] * numbers[counter + 1]; break;
+                    }
                 }
-                bigAnswer += answer;
+                    secondCounter++;
+                    bigAnswer += answer;                    
             }
             return bigAnswer;
-
         }
 
 
 
 
 
-        //searches for operator in the input
-        char operatorDetector(string input)
+        //produces list of operators from the users input
+        List<char> operatorDetector(string input)
         {
-            char c = 'n';
+
+            List<char> list = new List<char>();
             foreach (char i in input)
             {
                 switch (i)
                 {
                     case '-':
-                        c = i; break;
+                        list.Add(i); break;
                     case '+':
-                        c = i; break;
+                        list.Add(i); break;
                     case '/':
-                        c = i; break;
+                        list.Add(i); break;
                     case '*':
-                        c = i; break;
+                        list.Add(i); break;
                 }
             }
-            return c;
+            return list;
+            
         }
     }
 }
+
