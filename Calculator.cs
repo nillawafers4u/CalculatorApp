0f614ac84fc19a72c291c91cb186534f7f103cbe
Wafers <rsjonah@gmail.com>
2024-09-19 17:10:50 -0600

    Added Token Class

--- Calculator.cs
+++ Calculator.cs
@@ -1,77 +1,128 @@
 ﻿
 
-using System.Text.RegularExpressions;
-using System.Diagnostics.Metrics;
-using System.Linq.Expressions;
+using Microsoft.VisualBasic;
+using System.Diagnostics.SymbolStore;
 using System.Text.Json;
+using System.Text.RegularExpressions;
+namespace Calculator;
+
+
 
 internal class Program
 {
-    new FormatException 
+    
+
+    
+
+   
+
     private static void Main(string[] args)
     {
 
-        while(true) 
+
+        while (true)
         {
-            
+
             Console.WriteLine("please input a simple math problem. Examples: 5*5, 124/76, 4784+272 etc.");
 
-            //receives input from user
-            var input = Console.ReadLine();
-            var portion = input?[1..4];
-            new Token (TokenType.Number, 1, 1, 1);
+            // receives input from user
+            var inputString = Console.ReadLine();
+
+
             //program shutdown
-            if (input?.ToLower() is null or "exit")
+            if (inputString?.ToLower() is null or "exit")
                 break;
 
-            input = input.Trim();
-            var numberLength = GetNumberLength(input);
-            var result = double.Parse(input.Substring(0, numberLength));
-            input = input.Substring(numberLength).Trim();
-            while (input.Length > 0)
+            inputString = inputString.Trim();
+
+
+
+
+            for (int i = 0; i < inputString.Length; i++)
+
             {
-                var op = input[0];
-                input = input.Substring(1).Trim();
-                numberLength = GetNumberLength(input);
-                var operand = double.Parse(input.Substring(0, numberLength));
-                input = input.Substring(numberLength).Trim();
-                result = Calculate(result, op, operand);
-            }
+                var numberLength = GetNumberLength(inputString);
 
-        }
-
-
-        static double Calculate(double[] numbers, List<char> operators)
-        {
-            if (numbers.Length == 0)
-                return 0;
-
-            double result = numbers[0];
-
-            for (int i = 0; i < operators.Count; i++)
-            {
-                result = operators[i] switch
+                var result = double.Parse(inputString.Substring(0, numberLength));
+                inputString = inputString.Substring(numberLength).Trim();
+                while (inputString.Length > 0)
                 {
-                    '+' => result += numbers[i + 1],
-                    '-' => result -= numbers[i + 1],
-                    '*' => result *= numbers[i + 1],
-                    '/' => result /= numbers[i + 1],  
-                };
+                    char op = 'a';
+                    if (numberLength >= 1)
+                    {
+                        
+                        op = char.Parse(inputString.Substring(numberLength + 1, 1)) switch
+                        {
+                            '+' => op = '+',
+                            '-' => op = '-',
+                            '*' => op = '*',
+                            '/' => op = '/',
+                        };
+                    }
+
+                    inputString = inputString.Substring(1).Trim();
+                    numberLength = GetNumberLength(inputString);
+                    var operand = double.Parse(inputString.Substring(0, numberLength));
+                    inputString = inputString.Substring(numberLength).Trim();
+                
+                    result = Calculate(result, op, operand);
+                    
+                }
+
             }
 
-            return result;
+
+            static double Calculate(double result, char op, double operand)
+            {
+
+
+
+                result = op switch
+                {
+                    '+' => result += operand,
+                    '-' => result -= operand,
+                    '*' => result *= operand,
+                    '/' => result /= operand,
+                };
+
+
+                return result;
+            }
+
+
+
+
+
+            //extracts the numbers to an double array so that they can be worked
+            static double[] ExtractNumbers(string input) => Regex.Split(input, @"[-+*/]").Select(double.Parse).ToArray();
+
+
+
+            //produces list of operators from the users input
+            static List<char> ExtractOperators(string input) => input.Where(c => "+-*/".Contains(c)).ToList();
+
+            
+            
+            static int GetNumberLength(string input)
+            {
+                int result = 0;
+
+
+                foreach (char c in input)
+                {
+                    if (c is '+' or '-' or '/' or '*' or ' ')
+                    {
+                        break;
+                    }
+
+                    result += 1;
+                }
+
+                return result;
+
+
+            }
+
         }
     }
-
-
-        //extracts the numbers to an double array so that they can be worked
-        static double[] ExtractNumbers(string input) => Regex.Split(input, @"[-+*/]").Select(double.Parse).ToArray();
-        
-
-
-        //produces list of operators from the users input
-        static List<char> ExtractOperators(string input) => input.Where(c => "+-*/".Contains(c)).ToList();
-        
-
-    
 }
---
+++ Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Token
+    {
+        private enum TokenType
+        {
+            Nothing,
+            Number,
+            Operator,
+
+        };
+
+        // For all tokens
+        TokenType tokenType = TokenType.Nothing;
+
+        // If it's a number
+        double value = 0.0;
+
+        //If it's an operator
+        char symbol = ' ';
+
+        private Token() { }
+
+        public static Token StringToToken(string str)
+        {
+            Token t = new Token();
+
+            if (double.TryParse(str, out t.value))
+                t.tokenType = TokenType.Number;
+            else
+            {
+                switch (str)
+                {
+                    case "+":
+                        t.tokenType = TokenType.Operator;
+                        t.symbol = '+';
+                        break;
+
+                    case "-":
+                        t.tokenType = TokenType.Operator;
+                        t.symbol = '-';
+                        break;
+
+                    case "*":
+                        t.tokenType = TokenType.Operator;
+                        t.symbol = '*';
+                        break;
+
+                    case "/":
+                        t.tokenType = TokenType.Operator;
+                        t.symbol = '/';
+                        break;
+
+                    default:
+                        t.tokenType = TokenType.Nothing;
+                        break;
+                }
+            }
+            return t;
+        }
+
+
+    }
+}
